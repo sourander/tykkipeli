e4a84e88c70cd297dcdff658a01be2b159b78c52
sourander <31925965+sourander@users.noreply.github.com>
2017-10-26 20:42:57 +0300

    Networking Player Movement

---
+++ Tykkipeli/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerController : NetworkBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        // This is needed for Multiplayer
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        /// Template controls START
+        /// for network testing. REMOVE ->->
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+        transform.Translate(x, 0, 0);
+        transform.Translate(0, y, 0);
+        /// Template controls END
+
+    }
+}
