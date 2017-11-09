f514dc3ee2314760ce9522efe4c77116bb9ddeb1
sourander <31925965+sourander@users.noreply.github.com>
2017-11-09 16:32:13 +0200

    Cannon work for 1st time

--- Tykkipeli/Assets/Scripts/CannonRotation.cs
+++ Tykkipeli/Assets/Scripts/CannonRotation.cs
@@ -15,30 +15,57 @@ public class CannonRotation : NetworkBehaviour
     // public float minRotation;
     // public float maxRotation;
 
-    // Update is called once per frame
+    
     void Update()
     {
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        difference.Normalize(); //normalizing the vector. Meaning that all the sum of the vector will be equal to 1.
-
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; // find the angle in degrees
-        transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
-
-        /* if (rotZ > maxRotation)
+        // This is needed for Multiplayer
+        if (isLocalPlayer)
         {
-            transform.rotation = Quaternion.Euler(0f, 0f, maxRotation);
+        
+        
+
+
+            // Template controls START
+            // for network testing. REMOVE ->->
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
+            var y = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+            transform.Translate(x, 0, 0);
+            transform.Translate(0, y, 0);
+            // Template controls END
+            // 
+
+
+            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            difference.Normalize(); //normalizing the vector. Meaning that all the sum of the vector will be equal to 1.
+
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; // find the angle in degrees
+            transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+
+            /* if (rotZ > maxRotation)
+            {
+                transform.rotation = Quaternion.Euler(0f, 0f, maxRotation);
+            }
+
+            if (rotZ < minRotation)
+            {
+                transform.rotation = Quaternion.Euler(0f, 0f, minRotation);
+            } */
+
+            if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextShotTime)
+            {
+                CmdFire();
+            }
+
         }
+    }
 
-        if (rotZ < minRotation)
-        {
-            transform.rotation = Quaternion.Euler(0f, 0f, minRotation);
-        } */
+    [Command]
+    void CmdFire()
+    {
+        nextShotTime = Time.time + reloadRate;
+        var bullet = Instantiate(BulletPrefab, ShotSpawnTransform.position, Quaternion.identity) as GameObject;
+        bullet.GetComponent<Rigidbody2D>().velocity = transform.right * projectileSpeed;
 
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextShotTime)
-        {
-            nextShotTime = Time.time + reloadRate;
-            var bullet = Instantiate(BulletPrefab, ShotSpawnTransform.position, Quaternion.identity) as GameObject;
-            bullet.GetComponent<Rigidbody2D>().velocity = transform.right * projectileSpeed;
-        }
+        NetworkServer.Spawn(bullet);
     }
 }
