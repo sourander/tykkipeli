a8608cf54fb07a4ac3692cb5eca98c7d50b1221b
TaijaT <31925900+TaijaT@users.noreply.github.com>
2017-12-06 16:09:24 +0200

    minion move

---
+++ Tykkipeli/Assets/MinionMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionMove : MonoBehaviour
+{
+
+        public float moveSpeed;
+        bool touchground = false;
+        void Start()
+        {
+
+        }
+
+
+
+
+
+        void Update()
+        {
+
+            if (touchground == true)
+            {
+                transform.Translate(new Vector3(moveSpeed, 0, 0) * Time.deltaTime);
+            }
+        }
+
+
+        void OnCollisionEnter2D(Collision2D col)
+        {
+
+            if (col.gameObject.tag == "Boundary")
+            {
+                touchground = true;
+            }
+            if (col.gameObject.tag == "Palace")
+            {
+                moveSpeed *= -1;
+            }
+
+
+        }
+ }
